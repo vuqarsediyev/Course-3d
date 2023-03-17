037b999d2460173e318319abad1ffba92d710500
vuqarsediyev <114988093+vuqarsediyev@users.noreply.github.com>
2023-03-17 02:55:12 -0700

    Add files via upload

---
+++ 18_grid/js/script.js
@@ -0,0 +1,23 @@
+let burger = document.querySelector(".burger");
+let menu = document.querySelector('.header__nav');
+let menuLinks = document.querySelectorAll('.nav__item-link')
+
+burger.addEventListener('click', function () {
+
+    burger.classList.toggle('burger--active');
+
+    menu.classList.toggle('header__nav--active');
+
+    document.body.classList.toggle('stop-scroll')
+})
+
+menuLinks.forEach(function (e) {
+    e.addEventListener('click', function () {
+
+        burger.classList.remove('burger--active');
+
+        menu.classList.remove('header__nav--active');
+
+        document.body.classList.remove('stop-scroll')
+    })
+})
